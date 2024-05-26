d48bd8b289e02a45b0e6bcbbaa52282ba958cbfe
Angelina <mathswin1@gmail.com>
2024-05-26 16:25:32 +0400

    bhg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Items from "./components/Items";
---
+++ src/components/UserComponent.js
@@ -0,0 +1,32 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+//
+// function Test1() {
+//     const [data, setData] = useState([]);
+//
+//     useEffect(() => {
+//         axios
+//             .get("https://jsonplaceholder.typicode.com/users")
+//             .then((response) => {
+//                 setData(response.data);
+//             })
+//             .catch((error) => {
+//                 console.log(error);
+//             });
+//     }, []);
+//
+//     return (
+//         <div>
+//             <h1>Список пользователей:</h1>
+//             <ul>
+//                 {data.map((user) => (
+//                     <li key={user.id}>
+//                         {user.name} ({user.email})
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+//
+// export default Test1;
